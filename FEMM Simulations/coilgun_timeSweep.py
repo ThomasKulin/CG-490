1f6585bc9ae05af2c1f2fcabdebaaad6fe4cfdba
ekultho <thomas.kulin@ericsson.com>
2019-10-30 17:31:53 -0400

    started FEMM sim write-up, finished switch sim write-up

--- FEMM Simulations/coilgun_timeSweep.py
+++ FEMM Simulations/coilgun_timeSweep.py
@@ -7,8 +7,7 @@ import os
 
 # TEST PARAMS --- Units in cm
 # filename = "7mmCoil_192T_156A"
-# filename = "3.2mmCoil_100T_246A"
-filename = "14mmDia"
+filename = "3.2mmCoil_100T_246A"
 
 current = 246
 sensorPos = 0.005
